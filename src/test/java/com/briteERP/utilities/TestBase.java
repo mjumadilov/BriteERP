219ae373d181806527baf863680985494290fa18
comolokko13 <alinail13.ank@gmail.com>
2019-08-03 00:49:17 -0500

    first commit

---
+++ src/test/java/com/briteERP/utilities/TestBase.java
@@ -0,0 +1,53 @@
+package com.briteERP.utilities;
+
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.concurrent.TimeUnit;
+
+public class TestBase {
+
+    //should be public/protected !!!!
+    public WebDriver driver;
+    public Actions action;
+
+
+
+    @BeforeMethod
+    public void setup(){
+        driver = Driver.getDriver();
+        action = new Actions(driver);
+        driver.manage().timeouts().implicitlyWait(Long.valueOf(ConfigurationReader.getProperty("implicitwait")), TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get(ConfigurationReader.getProperty("url"));
+    }
+
+
+    //ITestresult describes the result of a test.
+    //we can determine if test failed, passed or ignored
+    @AfterMethod
+    public void teardown(){
+
+        Driver.closeDriver();
+    }
+
+    @AfterTest
+    public void tearDownTest(){
+        report.flush();
+    }
+}
